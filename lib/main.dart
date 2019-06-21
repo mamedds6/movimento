e56b5f388a0346a43373ab2d348e9d3bf576d263
mamedds6 <darekmichalski@onet.eu>
2019-06-21 13:36:43 +0200

    havingFun (animatedOpacity, setState, transform, raisedButton) crash if opacity > 1.0

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:camera/camera.dart';
 import 'dart:async';
+
+import 'package:flutter/widgets.dart';
 
 List<CameraDescription> cameras;
 
@@ -33,6 +36,8 @@ class CameraApp extends StatefulWidget {
 
 class _CameraAppState extends State<CameraApp> {
   CameraController controller;
+  var _myOpacity = 0.9;
+  var _myAngle = 0;
 
   @override
   void initState() {
@@ -42,7 +47,8 @@ class _CameraAppState extends State<CameraApp> {
       if (!mounted) {
         return;
       }
-      setState(() {});
+      setState(() {
+      });
     });
   }
 
@@ -54,6 +60,7 @@ class _CameraAppState extends State<CameraApp> {
 
   @override
 Widget build(BuildContext context) {
+
   return Scaffold(
     appBar: AppBar( 
       title: Text(widget.title)
@@ -68,19 +75,54 @@ Widget build(BuildContext context) {
                   child: new CameraPreview(controller)),
             ),
             new Positioned.fill(
-              child: new Opacity(
-                opacity: 0.3,
+              child: new AnimatedOpacity(
+                opacity: _myOpacity,
+                duration: new Duration(seconds: 3),
                 child: new Image.network(
                   'https://picsum.photos/3000/4000',
-                  fit: BoxFit.fill,
-                ),
-              ),
-            ),
+                  fit: BoxFit.fill
+                ),),),
+            new Positioned.fill(
+              child: new Stack(
+                children: <Widget>[
+                  new Positioned(
+                    bottom: 20,
+                    left: 20,
+                    height: 300,          
+                    child: FlutterLogo(),
+                  ),
+                  new Transform.rotate(
+                      angle: _myAngle*(2*3.14)/360,              
+                      origin: new Offset(0, 200),        
+                      child: new Text("data"),
+                    ),
+                  new RaisedButton(
+                    onPressed: () {
+                      setState(() {
+                        _myOpacity = 0.0;
+                      });
+                    },
+                    color: Color.fromARGB(111, 240, 235, 250),
+                    elevation: 30,
+                    highlightElevation: 80,
+                    disabledElevation: 20,
+                    clipBehavior: Clip.antiAlias,
+                    child: Text("button"),
+                  )
+                ],
+              ) 
+            )
+
           ],
         ),
       ),
-floatingActionButton: FloatingActionButton(
-        //onPressed: _incrementCounter,
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          setState(() {
+           _myOpacity +=0.1; 
+           _myAngle +=30;
+          });
+        },        
         tooltip: 'Increment',
         child: Icon(Icons.add),
       ),
